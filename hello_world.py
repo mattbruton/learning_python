42665f32e1e84e899be00043429ac7aceb95b024
MATT BRUTON <the.bruton@gmail.com>
2017-03-03 22:52:56 -0600

    completed first example from LPtHW

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,2 @@
-print("Hello World!")
+greeting = "Hello World!"
+print(greeting)
---
+++ print_stuff.py
@@ -0,0 +1,7 @@
+# print("Hello World!")
+# print("Hello Again!")
+# print("I like typing this!")
+# print("This is fun.")
+# print("Yay! Printing stuff!")
+# print("I'd rather you 'not'.")
+print('I "said" do not touch this.')
