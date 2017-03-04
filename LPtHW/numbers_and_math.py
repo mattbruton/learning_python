b6382bdbb2d1632e349a99c9a6d9ec762b830eed
MATT BRUTON <the.bruton@gmail.com>
2017-03-03 23:08:29 -0600

    completed exercise 3 of LPtHW, including variation on the commenting code section of exercise

---
+++ LPtHW/numbers_and_math.py
@@ -0,0 +1,28 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "I will now count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more?"
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# Lines 1, 6, 10, 17, 19 Simply print a string.
+# Lines 8, 12 Just do basic math and print the result.
+# Lines 21-23 Print a string, and print the result from comparing two equations.
+# All other lines print a string, then print the result of some assortment of operations.
