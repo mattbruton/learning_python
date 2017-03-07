2ec71c1a7fb36153f4978f914e0e45c78e307093
MATT BRUTON <the.bruton@gmail.com>
2017-03-06 23:45:31 -0600

    refactored two functions in random number game, created hangman-esque script file with initial requirements

---
+++ treehouse_stuff/hangman_game.py
@@ -0,0 +1,6 @@
+# Make a list of words
+# Pick random word from list
+# Draw spaces for each letter in selected word
+# Take guesses
+# Draw guessed letters / incorrect guesses
+# Print out win/lose
