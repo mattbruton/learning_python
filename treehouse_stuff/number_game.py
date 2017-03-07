769b5df6a90aebba6ad51e3f555df2c778f96794
MATT BRUTON <the.bruton@gmail.com>
2017-03-06 20:35:27 -0600

    added new function to make game slightly easier

--- treehouse_stuff/number_game.py
+++ treehouse_stuff/number_game.py
@@ -21,6 +21,10 @@ def handle_incorrect_guess(guess, remaining_attempts):
 def fail_to_win(selected_number):
     print('I\'m sorry! You failed to guess the secret number this time. It was {}.'.format(selected_number))
 
+def give_hint(guess, actual_number):
+     comparison_word = 'less' if actual_number < int(guess) else 'greater'
+     print('Hint: The number is {} than your guess.'.format(comparison_word))
+
 def check_guess_pattern(guess):
     result = re.match('^([1-9]|10){1}$', guess)
     if result:
@@ -29,26 +33,27 @@ def check_guess_pattern(guess):
         print("Please select a number 1 - 10.")
 
 def main():
-    attempts_remaining = 3
+    attempts_remaining = 2
     random_number = random.randint(1, 10)
-
     print(instructions)
+
     while True:
         # Prompt user for guess
         user_guess = input(">> ")
         # Check that guess is a number 1-9 or 10.
         # Will print message if pattern match fails.
         if check_guess_pattern(user_guess):
-            # Remove one attempt per guess
-            attempts_remaining -= 1
-            # Based on Guess:
+            # Based on guess:
             if user_guess == str(random_number):
                 # Correct guess, break loop, end game
                 handle_correct_guess(user_guess)
                 return False
             elif attempts_remaining >= 1:
-                # Incorrect guess, attempts still remaining
+                # Incorrect guess, tell user if number is higher or lower.
                 handle_incorrect_guess(user_guess, attempts_remaining)
+                give_hint(user_guess, random_number)
+                # Remove one attempt per incorrect guess
+                attempts_remaining -= 1
             else:
                 # No attempts left, game over
                 fail_to_win(random_number)
